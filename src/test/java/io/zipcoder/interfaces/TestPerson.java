27ff421be36abe5c794faf44d565a1f135fad017
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-07 13:28:01 -0500

    finished part one dot zero

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,34 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor() {
+        //given
+        Person expected = new Person(24, "Greg");
+        String expectedName = "Greg";
+        long expectedID = 24;
+        //when
+        String actualName = expected.getName();
+        long actualID = expected.getId();
+        //then
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedID, actualID);
+    }
+
+    @Test
+    public void testSetName() {
+        //given
+        Person expected = new Person(555, "Davis");
+        String expectedName = "Davis";
+        //when
+        expected.setName("Davis");
+        String actualName = expected.getName();
+        //then
+        Assert.assertEquals(expectedName, actualName);
+    }
 
 }
