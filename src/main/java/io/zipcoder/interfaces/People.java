7a7ee39b27b47112ea4bfaf4d54a152c14e6a4f8
Greg Davis <jrobley3@hotmail.com>
2020-03-07 23:26:42 -0500

    test commit from desktop

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,5 +1,27 @@
 package io.zipcoder.interfaces;
 
-public class People {
+import java.util.ArrayList;
+import java.util.List;
 
+public class People<E> {
+    List<E> personList = new ArrayList<E>();
+
+    public void add(E personToAdd){
+        personList.add(personToAdd);
+    }
+
+    public Person findById(long id){
+        return null;
+    }
+
+    public Boolean contains(Person person){
+        if (personList.contains(person)){
+            return true;
+        }
+        return false;
+    }
+
+    public void remove(Person person){
+        personList.remove(person);
+    }
 }
